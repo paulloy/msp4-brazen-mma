7c5c18be234feb74c326b58b9baf801498b807fd
paulloy <paulloy020896@gmail.com>
2021-02-22 19:59:43 +0000

    Initial commit. Django installed. Superuser created. Files added to .gitignore

---
+++ brazen_mma/asgi.py
@@ -0,0 +1,16 @@
+"""
+ASGI config for brazen_mma project.
+
+It exposes the ASGI callable as a module-level variable named ``application``.
+
+For more information on this file, see
+https://docs.djangoproject.com/en/3.1/howto/deployment/asgi/
+"""
+
+import os
+
+from django.core.asgi import get_asgi_application
+
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'brazen_mma.settings')
+
+application = get_asgi_application()
