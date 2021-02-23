e2822591a34ce8227cf19d779d8b958593341e78
paulloy <paulloy020896@gmail.com>
2021-02-23 13:33:34 +0000

    Security error fixed: Django SECRET_KEY was committed to repo. env.py file containing a new secret key has been added to .gitignore and a new hidden SECRET_KEY has been added in settings.py

--- brazen_mma/settings.py
+++ brazen_mma/settings.py
@@ -12,6 +12,7 @@ https://docs.djangoproject.com/en/3.1/ref/settings/
 
 from pathlib import Path
 import os
+from . import env
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -21,7 +22,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'p#71)=v=ow__6acl!ntrn_e0d)eizau6_n)0&!)%7r7xwl_$8d'
+SECRET_KEY = os.environ.get('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
