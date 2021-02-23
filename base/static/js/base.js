75a4d464a34aa597ba4a36124a85510c0d0c2198
paulloy <paulloy020896@gmail.com>
2021-02-23 19:11:03 +0000

    Templates for tablet and mobile added. Base template is now responsive. Script added for toggling menu and submenus

--- base/static/js/base.js
+++ base/static/js/base.js
@@ -1,3 +1,25 @@
 $('#product-menu ul li').click(function() {
     $(this).find('.product-menu-submenu').toggle();
 });
+
+let navOpen = false;
+$('#navbar-toggle, #navbar-toggle-mobile').click(function() {
+    if (navOpen === false) {
+        $('#product-menu-tablet').show().animate({
+            right: 0
+        }, 300, function() {
+            navOpen = true;
+        });
+    } else {
+        $('#product-menu-tablet').animate({
+            right: '-12.5rem'
+        }, 300, function() {
+            navOpen = false;
+            $('#product-menu-tablet').hide();
+        });
+    }
+});
+
+$('#product-menu-tablet ul li').click(function() {
+    $(this).find('.product-menu-submenu').toggle();
+});
