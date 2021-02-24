9fc158276cee7a240d62986e8b5036f1aaa7b25c
paulloy <paulloy020896@gmail.com>
2021-02-24 19:12:27 +0000

    Create models and admin classes for products. Add placeholder image if product has no image. make migrations on model. Add a product to test that model works as intended

--- products/admin.py
+++ products/admin.py
@@ -1,3 +1,24 @@
 from django.contrib import admin
+from .models import (Product, ProductCategory,
+                     ProductType, ProductGender, ProductSizesStock)
 
-# Register your models here.
+
+class ProductSizesStockAdminInline(admin.TabularInline):
+    model = ProductSizesStock
+    readonly_fields = ('product_id',)
+
+
+class ProductAdmin(admin.ModelAdmin):
+    inlines = (ProductSizesStockAdminInline,)
+
+    list_display = ('product_id', 'name', 'price', 'sale')
+
+    readonly_fields = ('product_id',)
+
+    ordering = ('product_id',)
+
+
+admin.site.register(ProductCategory)
+admin.site.register(ProductType)
+admin.site.register(ProductGender)
+admin.site.register(Product, ProductAdmin)
--- products/models.py
+++ products/models.py
@@ -1,3 +1,77 @@
 from django.db import models
+import uuid
 
-# Create your models here.
+
+class ProductCategory(models.Model):
+    product_category_id = models.AutoField(primary_key=True)
+    name = models.CharField(max_length=254)
+
+    def __str__(self):
+        return self.name
+
+    class Meta:
+        verbose_name_plural = 'Product Categories'
+
+
+class ProductType(models.Model):
+    product_type_id = models.AutoField(primary_key=True)
+    name = models.CharField(max_length=254)
+
+    def __str__(self):
+        return self.name
+
+    class Meta:
+        verbose_name_plural = 'Product Types'
+
+
+class ProductGender(models.Model):
+    product_gender_id = models.AutoField(primary_key=True)
+    name = models.CharField(max_length=254)
+
+    def __str__(self):
+        return self.name
+
+    class Meta:
+        verbose_name_plural = 'Product Genders'
+
+
+class Product(models.Model):
+    product_id = models.UUIDField(
+        primary_key=True, default=uuid.uuid4, editable=False)
+    name = models.CharField(max_length=50, blank=False, null=False)
+    description = models.TextField(null=False, blank=False)
+    gender = models.ForeignKey(
+        'ProductGender', on_delete=models.CASCADE, null=True, blank=False)
+    sale = models.BooleanField(null=False, blank=False, default=False)
+    rrp = models.DecimalField(
+        verbose_name='Recommended Retail Price',
+        max_digits=6, decimal_places=2, blank=False, null=False)
+    price = models.DecimalField(max_digits=6, decimal_places=2)
+    category = models.ForeignKey(
+        'ProductCategory', on_delete=models.CASCADE,
+        blank=False, null=True)
+    product_type = models.ForeignKey(
+        'ProductType', on_delete=models.CASCADE,
+        blank=False, null=True)
+    image = models.ImageField(upload_to='products/static/product_img',
+                              blank=False, null=False,
+                              default='products/static/\
+                                  product_img/placeholder.jpg')
+
+    def __str__(self):
+        return self.name
+
+
+class ProductSizesStock(models.Model):
+    product_sizes_quantity_id = models.AutoField(primary_key=True)
+    product_id = models.ForeignKey(
+        'Product', on_delete=models.CASCADE, null=True)
+    size = models.CharField(max_length=254)
+    stock = models.PositiveIntegerField()
+
+    def __str__(self):
+        my_tuple = (str(self.product_id), str(self.size))
+        return ' | '.join(my_tuple)
+
+    class Meta:
+        verbose_name_plural = 'Product Sizes & Quantities'
