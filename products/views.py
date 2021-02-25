a5e7a79d4a1c56bc84eab0e30e3550e8dee5937f
paulloy <paulloy020896@gmail.com>
2021-02-25 14:34:35 +0000

    Create products view and template. CSS temporarily set on products.html, add to static file when page is being properly styled

--- products/views.py
+++ products/views.py
@@ -1,7 +1,14 @@
 from django.shortcuts import render
+from .models import Product
 
 
 def all_products(request):
     """ A view to return the index page """
 
-    return render(request, 'products/products.html')
+    products = Product.objects.all()
+
+    context = {
+        'products': products,
+    }
+
+    return render(request, 'products/products.html', context)
