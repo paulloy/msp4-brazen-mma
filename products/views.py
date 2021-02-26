bd14e78b55998e3bd1531d4fbbccb932d9cbcffb
paulloy <paulloy020896@gmail.com>
2021-02-26 20:07:51 +0000

    Add search form to base template, and mobile/tablet header templates, so that a user can search directly for a product via a search form

--- products/views.py
+++ products/views.py
@@ -1,5 +1,6 @@
-from django.shortcuts import render, get_object_or_404
-from .models import Product
+from django.shortcuts import render, get_object_or_404, redirect, reverse
+from django.db.models import Q
+from .models import Product, ProductSizesStock
 
 
 def all_products(request):
@@ -8,6 +9,7 @@ def all_products(request):
     products = Product.objects.all()
     category = None
     product_type = None
+    query = None
 
     if request.GET:
         if 'category' and 'product_type' in request.GET:
@@ -20,10 +22,21 @@ def all_products(request):
             category = request.GET['category']
             products = products.filter(category__name__contains=category)
 
+        if 'q' in request.GET:
+            query = request.GET['q']
+            if not query:
+                # messages.error(
+                #     request, "You didn't enter any search criteria!")
+                return redirect(reverse('products'))
+
+            queries = Q(name__icontains=query)
+            products = products.filter(queries)
+
     context = {
         'products': products,
         'category': category,
         'product_type': product_type,
+        'search_term': query,
     }
 
     return render(request, 'products/products.html', context)
@@ -33,9 +46,16 @@ def product_details(request, product_id):
     """ A view to return an individual products details """
 
     product = get_object_or_404(Product, pk=product_id)
+    sizes = ProductSizesStock.objects.filter(product_id=product_id)
+    save = None
+
+    if product.rrp != product.price:
+        save = product.rrp - product.price
 
     context = {
         'product': product,
+        'sizes': sizes,
+        'save': save,
     }
 
     return render(request, 'products/product_details.html', context)
