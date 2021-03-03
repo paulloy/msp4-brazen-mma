4a7835625458b127e82872270bff7c45beb0a2c2
paulloy <paulloy020896@gmail.com>
2021-03-03 14:46:35 +0000

    Redundant models removed, ForeignKey replaced with CharField dropdown menu. list_display updated to include gender, category, and product_type

--- products/admin.py
+++ products/admin.py
@@ -1,6 +1,5 @@
 from django.contrib import admin
-from .models import (Product, ProductCategory,
-                     ProductType, ProductGender, ProductSizesStock)
+from .models import Product, ProductSizesStock
 
 
 class ProductSizesStockAdminInline(admin.TabularInline):
@@ -11,14 +10,13 @@ class ProductSizesStockAdminInline(admin.TabularInline):
 class ProductAdmin(admin.ModelAdmin):
     inlines = (ProductSizesStockAdminInline,)
 
-    list_display = ('product_id', 'name', 'price', 'sale')
+    list_display = (
+        'product_id', 'name', 'category',
+        'product_type', 'gender', 'price', 'sale')
 
     readonly_fields = ('product_id',)
 
     ordering = ('product_id',)
 
 
-admin.site.register(ProductCategory)
-admin.site.register(ProductType)
-admin.site.register(ProductGender)
 admin.site.register(Product, ProductAdmin)
--- products/models.py
+++ products/models.py
@@ -2,37 +2,39 @@ from django.db import models
 import uuid
 
 
-class ProductCategory(models.Model):
-    product_category_id = models.AutoField(primary_key=True)
-    name = models.CharField(max_length=254)
+PRODUCT_CATEGORY_CHOICES = (
+    ('select category', 'SELECT CATEGORY'),
+    ('bjj', 'BJJ'),
+    ('mma', 'MMA'),
+    ('muay thai', 'MUAY THAI'),
+    ('equipment', 'EQUIPMENT'),
+)
 
-    def __str__(self):
-        return self.name
+PRODUCT_TYPE_CHOICES = (
+    ('select type', 'SELECT TYPE'),
+    ('shorts', 'SHORTS'),
+    ('gloves', 'GLOVES'),
+    ('shin guards', 'SHIN GUARDS'),
+    ('mouth guards', 'MOUTH GUARDS'),
+    ('handwraps', 'HANDWRAPS'),
+    ('gi', 'GI'),
+    ('belts', 'BELTS'),
+    ('rash guards', 'RASH GUARDS'),
+    ('spats', 'SPATS'),
+    ('groin guards', 'GROIN GUARDS'),
+    ('kick shields', 'KICK SHIELDS'),
+    ('belly pads', 'BELLY PADS'),
+    ('punch bags', 'PUNCH BAGS'),
+    ('grappling dummies', 'GRAPPLING DUMMIES'),
+    ('joint support', 'JOINT SUPPORT'),
+)
 
-    class Meta:
-        verbose_name_plural = 'Product Categories'
-
-
-class ProductType(models.Model):
-    product_type_id = models.AutoField(primary_key=True)
-    name = models.CharField(max_length=254)
-
-    def __str__(self):
-        return self.name
-
-    class Meta:
-        verbose_name_plural = 'Product Types'
-
-
-class ProductGender(models.Model):
-    product_gender_id = models.AutoField(primary_key=True)
-    name = models.CharField(max_length=254)
-
-    def __str__(self):
-        return self.name
-
-    class Meta:
-        verbose_name_plural = 'Product Genders'
+GENDER_CHOICES = (
+    ('select gender', 'SELECT GENDER'),
+    ('men', 'MEN'),
+    ('women', 'WOMEN'),
+    ('unisex', 'UNISEX'),
+)
 
 
 class Product(models.Model):
@@ -40,22 +42,21 @@ class Product(models.Model):
         primary_key=True, default=uuid.uuid4, editable=False)
     name = models.CharField(max_length=254, blank=False, null=False)
     description = models.TextField(null=False, blank=False)
-    gender = models.ForeignKey(
-        'ProductGender', on_delete=models.CASCADE, null=True, blank=False)
+    gender = models.CharField(
+        max_length=20, choices=GENDER_CHOICES, default="SELECT GENDER")
     sale = models.BooleanField(null=False, blank=False, default=False)
     rrp = models.DecimalField(
         verbose_name='Recommended Retail Price',
         max_digits=6, decimal_places=2, blank=False, null=False)
     price = models.DecimalField(max_digits=6, decimal_places=2)
-    category = models.ForeignKey(
-        'ProductCategory', on_delete=models.CASCADE,
-        blank=False, null=True)
-    product_type = models.ForeignKey(
-        'ProductType', on_delete=models.CASCADE,
-        blank=False, null=True)
+    category = models.CharField(
+        max_length=20, choices=PRODUCT_CATEGORY_CHOICES,
+        default="SELECT CATEGORY")
+    product_type = models.CharField(
+        max_length=20, choices=PRODUCT_TYPE_CHOICES, default="SELECT TYPE")
     image = models.ImageField(upload_to='product_img/',
                               blank=False, null=False,
-                              default='products/static/\
+                              default='products/static/ \
                                   product_img/placeholder.jpg')
 
     def __str__(self):
