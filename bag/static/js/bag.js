8868d3e8eecdb66df83a65e09a9b2cace83e1684
paulloy <paulloy020896@gmail.com>
2021-03-09 19:22:47 +0000

    Refactor: Product quantities are now displayed from the context. Redundant JS removed. Rest of the JS has been added to an external static file

---
+++ bag/static/js/bag.js
@@ -0,0 +1,16 @@
+var $displayQuantity = $('.display-quantity');
+
+$('.quantity-add').click(function() {
+    var val = $(this).prev().val();
+    if (val < 99) {
+        val = parseInt(val) + 1;
+        $(this).prev().val(val);
+    }
+});
+$('.quantity-sub').click(function() {
+    var val = $(this).next().val();
+    if (val > 1) {
+        val = parseInt(val) - 1;
+        $(this).next().val(val);
+    }
+});
