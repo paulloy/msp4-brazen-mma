3f67c1da3519945d641715791f86a68b05b0aa89
paulloy <paulloy020896@gmail.com>
2021-03-09 17:31:09 +0000

    A user can now adjust the quantity of an item in their bag

--- bag/urls.py
+++ bag/urls.py
@@ -4,6 +4,7 @@ from . import views
 urlpatterns = [
     path('', views.view_bag, name='view_bag'),
     path('add/<product_id>/', views.add_to_bag, name='add_to_bag'),
+    path('adjust/<product_id>/', views.adjust_bag, name='adjust_bag'),
     path(
         'remove/<product_id>/', views.remove_from_bag, name='remove_from_bag'),
 ]
--- bag/views.py
+++ bag/views.py
@@ -1,4 +1,6 @@
-from django.shortcuts import render, redirect, reverse, HttpResponse
+from django.shortcuts import render, redirect, reverse
+from django.contrib import messages
+from products.views import Product
 
 
 def view_bag(request):
@@ -10,6 +12,7 @@ def view_bag(request):
 def add_to_bag(request, product_id):
     """ Add a product to the bag with its quantity """
 
+    product = Product.objects.get(pk=product_id)
     quantity = int(request.POST.get('quantity'))
     redirect_url = request.POST.get('redirect_url')
     size = None
@@ -34,7 +37,33 @@ def add_to_bag(request, product_id):
 
     request.session['bag'] = bag
 
+    messages.success(request, f'{product.name} | {size} added to bag.')
+
     return redirect(redirect_url)
+
+
+def adjust_bag(request, product_id):
+    """ update quantity of products in bag """
+
+    product = Product.objects.get(pk=product_id)
+    quantity = int(request.POST.get('quantity'))
+    size = None
+    if 'size' in request.POST:
+        size = request.POST['size']
+    bag = request.session.get('bag', {})
+
+    if size:
+        if product_id in list(bag.keys()):
+            if size in bag[product_id]['product_size'].keys():
+                bag[product_id]['product_size'][size] = quantity
+        else:
+            bag[product_id] = {'product_size': {size: quantity}}
+
+    request.session['bag'] = bag
+
+    messages.success(request, f'{product.name} | {size} quantity adjusted.')
+
+    return redirect(reverse('view_bag'))
 
 
 def remove_from_bag(request, product_id):
