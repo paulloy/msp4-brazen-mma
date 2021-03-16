1347f017c82b66f4939dc89441699292b1882186
paulloy <paulloy020896@gmail.com>
2021-03-16 13:08:38 +0000

    Added profile forms and updated views, models, and template

--- profiles/views.py
+++ profiles/views.py
@@ -1,5 +1,7 @@
 from django.shortcuts import render, get_object_or_404
+from django.contrib import messages
 from .models import UserProfile
+from .forms import UserProfileForm
 
 
 def profile(request):
@@ -7,9 +9,19 @@ def profile(request):
 
     profile = get_object_or_404(UserProfile, user=request.user)
 
+    if request.method == 'POST':
+        form = UserProfile(request.POST, instance=profile)
+        if form.is_valid():
+            form.save()
+            messages.success(request, 'Profile updated.')
+
+    form = UserProfileForm(instance=profile)
+    orders = profile.orders.all()
+
     template = 'profiles/profile.html'
     context = {
-        'profile': profile,
+        'form': form,
+        'orders': orders,
     }
 
     return render(request, template, context)
