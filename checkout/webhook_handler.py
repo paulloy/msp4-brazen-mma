87939a57501ff29a1b42989933c37779e8c1c21b
paulloy <paulloy020896@gmail.com>
2021-03-16 16:18:27 +0000

    Set up a url for Stripe webhooks. Webhook_handler handles webhook events and webhook is the function called by the webhook

---
+++ checkout/webhook_handler.py
@@ -0,0 +1,30 @@
+from django.http import HttpResponse
+
+
+class StripeWH_Handler:
+
+    def __init__(self, request):
+        self.request = request
+
+    def handle_event(self, event):
+        """ handle generic/unknown/unexpected webhook """
+        return HttpResponse(
+            content=f'Unhandled webhook received: {event["type"]}',
+            status=200
+        )
+
+    def handle_payment_intent_succeeded(self, event):
+        """ handle payment_intent.succeeded webhook from stripe """
+        intent = event.data.object
+        print(intent)
+        return HttpResponse(
+            content=f'Webhook received: {event["type"]}',
+            status=200
+        )
+
+    def handle_payment_intent_payment_failed(self, event):
+        """ handle payment_intent.failed webhook from stripe """
+        return HttpResponse(
+            content=f'Webhook received: {event["type"]}',
+            status=200
+        )
