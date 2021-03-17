f79787f466e0e337200567a145d101e71be701d3
paulloy <paulloy020896@gmail.com>
2021-03-17 16:53:19 +0000

    Products can be deleted. Buttons for editing and deleting products have been added to product_details.html template

--- products/urls.py
+++ products/urls.py
@@ -6,4 +6,6 @@ urlpatterns = [
     path('<uuid:product_id>/', views.product_details, name='product_details'),
     path('add/', views.add_product, name='add_product'),
     path('edit/<uuid:product_id>', views.edit_product, name='edit_product'),
+    path('delete/<uuid:product_id>',
+         views.delete_product, name='delete_product'),
 ]
