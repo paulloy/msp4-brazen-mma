695f495bf2e6e8bf2c85a68dbda63fdb037b91e0
paulloy <paulloy020896@gmail.com>
2021-03-21 20:28:30 +0000

    File has been updated so that the failed tests from TESTING.md now pass

--- bag/views.py
+++ bag/views.py
@@ -12,32 +12,38 @@ def view_bag(request):
 def add_to_bag(request, product_id):
     """ Add a product to the bag with its quantity """
 
-    product = Product.objects.get(pk=product_id)
-    quantity = int(request.POST.get('quantity'))
-    redirect_url = request.POST.get('redirect_url')
-    size = None
-    if 'size' in request.POST:
-        size = request.POST['size']
+    """
+        If a user accesses this function by url,
+        redirect them to the home page
+    """
+    try:
+        product = Product.objects.get(pk=product_id)
+        quantity = int(request.POST.get('quantity'))
+        redirect_url = request.POST.get('redirect_url')
+    except Exception:
+        messages.error(request, '403 Forbidden')
+        return redirect(reverse('home'))
+
+    size = request.POST['size']
     bag = request.session.get('bag', {})
 
-    if size:
-        if product_id in list(bag.keys()):
-            if size in bag[product_id]['product_size'].keys():
-                bag[product_id]['product_size'][size] += quantity
-            else:
-                bag[product_id]['product_size'][size] = quantity
+    if product_id in list(bag.keys()):
+        if size in bag[product_id]['product_size'].keys():
+            bag[product_id]['product_size'][size] += quantity
         else:
-            bag[product_id] = {'product_size': {size: quantity}}
-
-    elif size == 'false':
-        if product_id in list(bag.keys()):
-            bag[product_id] += quantity
-        else:
-            bag[product_id] = quantity
+            bag[product_id]['product_size'][size] = quantity
+    else:
+        bag[product_id] = {'product_size': {size: quantity}}
 
     request.session['bag'] = bag
 
-    messages.success(request, f'{product.name} | {size} added to bag.')
+    if size == 'false':
+        messages.success(
+            request, f'<strong>{product.name}</strong> | added to bag.')
+    else:
+        messages.success(
+            request, f'<strong>{product.name}</strong> | Size: <strong>{size}</strong>\
+                 | added to bag.')
 
     return redirect(redirect_url)
 
@@ -45,23 +51,43 @@ def add_to_bag(request, product_id):
 def adjust_bag(request, product_id):
     """ update quantity of products in bag """
 
-    product = Product.objects.get(pk=product_id)
-    quantity = int(request.POST.get('quantity'))
-    size = None
-    if 'size' in request.POST:
-        size = request.POST['size']
     bag = request.session.get('bag', {})
 
-    if size:
-        if product_id in list(bag.keys()):
-            if size in bag[product_id]['product_size'].keys():
-                bag[product_id]['product_size'][size] = quantity
-        else:
-            bag[product_id] = {'product_size': {size: quantity}}
+    """ Check that the product exists in bag """
+    try:
+        bag[product_id]
+    except Exception:
+        messages.error(request, '403 Forbidden')
+        return redirect(reverse('home'))
+
+    """ Check that product exists in database """
+    try:
+        product = Product.objects.get(pk=product_id)
+    except Exception:
+        messages.error(request, 'Bad request: Product ID is invalid.')
+        return redirect(reverse('view_bag'))
+
+    """ Check that quantity is of type 'int' """
+    try:
+        quantity = int(request.POST.get('quantity'))
+    except Exception:
+        messages.error(
+            request, 'Bad request: Input must be an integer.')
+        return redirect(reverse('view_bag'))
+
+    size = request.POST['size']
+
+    bag[product_id]['product_size'][size] = quantity
 
     request.session['bag'] = bag
 
-    messages.success(request, f'{product.name} | {size} quantity adjusted.')
+    if size == 'false':
+        messages.success(
+            request, f'<strong>{product.name}</strong> | quantity updated.')
+    else:
+        messages.success(
+            request, f'<strong>{product.name}</strong> | Size: \
+                <strong>{size}</strong> | quantity updated.')
 
     return redirect(reverse('view_bag'))
 
@@ -69,15 +95,35 @@ def adjust_bag(request, product_id):
 def remove_from_bag(request, product_id):
     """ Remove product from bag """
 
-    bag = request.session.get('bag', {})
-    size = request.POST['size']
+    """
+        If a user accesses this function by url,
+        redirect them to the home page
+    """
+    try:
+        product = Product.objects.get(pk=product_id)
+        bag = request.session.get('bag', {})
+        size = request.POST['size']
+    except Exception:
+        messages.error(request, '403 Forbidden.')
+        return redirect(reverse('home'))
 
-    del bag[product_id]['product_size'][size]
+    """ Try delete bag item. Return error if item does not exist """
+    try:
+        del bag[product_id]['product_size'][size]
+    except Exception:
+        messages.error(
+            request, 'Product cannot be removed. \
+                Product does not exist in bag.')
+        return redirect(reverse('view_bag'))
 
-    if not bag[product_id]['product_size']:
-        bag.pop(product_id)
+    if size == 'false':
+        messages.success(
+            request, f'<strong>{product.name}</strong> | removed from bag.')
+    else:
+        messages.success(
+            request, f'<strong>{product.name}</strong> | Size: <strong>\
+                {size}</strong> | removed from bag.')
 
     request.session['bag'] = bag
 
     return redirect(reverse('view_bag'))
-
