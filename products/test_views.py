b8182e0c6fbda68663c2d596a61d607cbdfff907
paulloy <paulloy020896@gmail.com>
2021-03-27 16:02:18 +0000

    Views tested with 61% coverage

--- products/test_views.py
+++ products/test_views.py
@@ -1,74 +1,94 @@
 from django.test import TestCase, RequestFactory, Client
 from django.shortcuts import HttpResponse
 from django.urls import reverse
+from django.contrib.auth.models import User
+
 from . import models
-from . import views
+from .models import Product, ProductSizesStock
+
 import random
 
 
 class TestViews(TestCase):
 
     def setUp(self):
-        # Some tests need access to the request factory.
-        self.factory = RequestFactory()
+        self.request_factory = RequestFactory()
+        self.user = User.objects.create_user(
+            username='user', email='user@example.com', password='password')
 
-    # all_products()
-
+    """ all_products() """
     def test_all_products_view(self):
         response = self.client.get('/products/')
         self.assertEqual(response.status_code, 200)
         self.assertTemplateUsed(response, 'products/products.html')
 
     def test_category_and_product_type_in_get_request(self):
-        c = Client()
         category = random.sample(models.PRODUCT_CATEGORY_CHOICES, k=1)[0][0]
         product_type = random.sample(models.PRODUCT_TYPE_CHOICES, k=1)[0][0]
-        response = c.get(
+        response = self.client.post(
             f'/products/?category={category}&product_type={product_type}')
         self.assertEqual(response.status_code, 200)
-        self.assertTemplateUsed(response, 'products/products.html')
 
     def test_category_in_get_request(self):
-        c = Client()
         category = random.sample(models.PRODUCT_CATEGORY_CHOICES, k=1)[0][0]
-        response = c.get(f'/products/?category={category}')
+        response = self.client.post(
+            f'/products/?category={category}')
         self.assertEqual(response.status_code, 200)
-        self.assertTemplateUsed(response, 'products/products.html')
 
     def test_q_in_get_request(self):
-        c = Client()
-        response = c.get('/products/?q=user_custom_value')
+        response = self.client.post(
+            f'/products/?q=searched_value')
         self.assertEqual(response.status_code, 200)
-        self.assertTemplateUsed(response, 'products/products.html')
 
     def test_sort_and_direction_in_get_request(self):
-        c = Client()
         sort = ['price', 'name']
         direction = ['asc', 'desc']
         for sortkey in sort:
             for directionkey in direction:
-                response = c.get(
+                response = self.client.post(
                     f'/products/?sort={sortkey}&direction={directionkey}')
                 self.assertEqual(response.status_code, 200)
-                self.assertTemplateUsed(response, 'products/products.html')
 
     def test_filters_in_get_request(self):
-        c = Client()
         filters = ['MEN', 'WOMEN', 'UNISEX']
         for filter_key in filters:
-            response = c.get(f'/products/?filters={filter_key}')
+            response = self.client.post(
+                f'/products/?filters={filter_key}')
             self.assertEqual(response.status_code, 200)
-            self.assertTemplateUsed(response, 'products/products.html')
 
-    # product_details()
+    """ product_details() """
 
     def test_product_details_view(self):
-        product = models.Product.objects.create(
-            name='Test Product', price='0.00', rrp='1.00')
-        sizes_no_stock = models.ProductSizesStock.objects.create(
-            product_id=product, size='Test Size Stock 0', stock=0)
-        sizes_less_than_five = models.ProductSizesStock.objects.create(
-            product_id=product, size='Test Size Stock < 5', stock=3)
-        response = self.client.get(f'/products/{product.product_id}')
+        """ create product """
+        product = Product.objects.create(
+            name='Test Product', price='5.00', rrp='10.00')
+
+        """ add sizes to product """
+        """ stock <=5  """
+        ProductSizesStock.objects.create(
+            product_id=product, size="test_size", stock=3)
+        """ stock = 0 """
+        ProductSizesStock.objects.create(
+            product_id=product, size="test_size_2", stock=0)
+
+        response = self.client.get(f'/products/{product.product_id}/')
         self.assertEqual(response.status_code, 200)
-        self.assertTemplateUsed(response, 'products/product_details.html')
+
+    def test_product_size_false(self):
+        """ create product """
+        product = Product.objects.create(
+            name='Test Product', price='5.00', rrp='10.00')
+
+        """ add sizes to product """
+        """ size false  """
+        ProductSizesStock.objects.create(
+            product_id=product, size="false", stock=3)
+
+        response = self.client.get(f'/products/{product.product_id}/')
+        self.assertEqual(response.status_code, 200)
+
+    """ ajax_q_request() """
+    def test_ajax_q_request(self):
+        response = self.client.post('/products/ajax_q/?q=bjj')
+        print(response)
+        self.assertEqual(response.status_code, 200)
